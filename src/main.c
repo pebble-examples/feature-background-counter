b1bbec3c256169b6166326ba1141e7385d28870d
Mitchell Roy <mitchellroy@A1246-MBP15.hq.getpebble.com>
2015-10-26 16:26:57 -0700

    code review changes

--- src/main.c
+++ src/main.c
@@ -1,9 +1,6 @@
 #include <pebble.h>
 
 #define WORKER_TICKS 0
-#define INSET 8
-#define TICKS_LAYER_ALIGNMENT PBL_IF_RECT_ELSE(GTextAlignmentLeft, GTextAlignmentCenter)
-#define X_PADDING PBL_IF_RECT_ELSE(5, 0)
 
 static Window *s_main_window;
 static TextLayer *s_output_layer, *s_ticks_layer;
@@ -54,6 +51,7 @@ static void click_config_provider(void *context) {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  const int inset = 8;
 
   // Create UI
   s_output_layer = text_layer_create(bounds);
@@ -61,15 +59,16 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 #ifdef PBL_SDK_3
-  text_layer_enable_screen_text_flow_and_paging(s_output_layer, INSET);
+  text_layer_enable_screen_text_flow_and_paging(s_output_layer, inset);
 #endif
 
-  s_ticks_layer = text_layer_create(GRect(X_PADDING, 135, bounds.size.w, 30));
+  s_ticks_layer = text_layer_create(GRect(PBL_IF_RECT_ELSE(5, 0), 135, bounds.size.w, 30));
   text_layer_set_text(s_ticks_layer, "No data yet.");
-  text_layer_set_text_alignment(s_ticks_layer, TICKS_LAYER_ALIGNMENT);
+  text_layer_set_text_alignment(s_ticks_layer, PBL_IF_RECT_ELSE(GTextAlignmentLeft, 
+                                                                GTextAlignmentCenter));
   layer_add_child(window_layer, text_layer_get_layer(s_ticks_layer));
 #ifdef PBL_SDK_3
-  text_layer_enable_screen_text_flow_and_paging(s_ticks_layer, INSET);
+  text_layer_enable_screen_text_flow_and_paging(s_ticks_layer, inset);
 #endif
 }
 
